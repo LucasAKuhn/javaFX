f9c8f26fc12d50f8fac3b9b1b09dc23ebc91f538
LucasAKuhn <lucasa.kuhn@hotmail.com>
2022-06-28 09:41:44 -0300

    aula 390

--- src/fxml/LoginControlador.java
+++ src/fxml/LoginControlador.java
@@ -1,5 +1,18 @@
 package fxml;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.PasswordField;
+
 public class LoginControlador {
 
+	@FXML
+	private javafx.scene.control.TextField campoEmail;
+	
+	@FXML
+	private PasswordField campoSenha;
+	
+	public void entrar() {
+		System.out.println(campoEmail.getText());
+		System.out.println(campoSenha.getText());
+	}
 }
